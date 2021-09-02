0c0f96acbfc9059d0cc28f9691073ec9c22980c1
Christian Vega-Munguia <christianvega.cvm@gmail.com>
2021-09-01 19:18:16 -0500

    added zombie counter

--- contracts/Zombies.sol
+++ contracts/Zombies.sol
@@ -14,8 +14,13 @@ contract ZombieFactory {
 
     Zombie[] public zombies;
 
+    mapping (uint => address) public zombieToOwner;
+    mapping (address => uint) ownerZombieCount;
+
     function _createZombie(string memory _name, uint _dna) private {
         uint id = zombies.push(Zombie(_name, _dna)) - 1;
+        zombieToOwner[id] = msg.sender;
+        ownerZombieCount[msg.sender]++;
         emit NewZombie(id, _name, _dna);
     }
 
@@ -25,6 +30,7 @@ contract ZombieFactory {
     }
 
     function createRandomZombie(string memory _name) public {
+        require(ownerZombieCount[msg.sender] == 0);
         uint randDna = _generateRandomDna(_name);
         _createZombie(_name, randDna);
     }
